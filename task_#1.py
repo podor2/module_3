4b49e90d5ccde30be25796258953a8733e604169
Andrii Podrovan <a.ndrey76907@gmail.com>
2024-08-27 17:40:53 +0300

    Corrected fucntions for homework#3

--- task_#1.py
+++ task_#1.py
@@ -1,18 +1,17 @@
 import datetime
-import re
-
 
 def get_days_from_today(date):
 
-    input_date = re.search(r'(\d{4})\-(\d{2})\-(\d{2})', date)
-    if input_date:
-        input_date = datetime.date(year=int(input_date.group(1)), month=int(
-            input_date.group(2)), day=int(input_date.group(3)))
-    else:
-        print(
-            'The date format is not correct. Please use format "YYYY-MM-DD" and try again')
+    try :
+        input_date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+        current_date = datetime.datetime.now().date()
+        diff_in_days = (current_date - input_date).days
+        return diff_in_days
 
-    current_date = datetime.datetime.now().date()
+    except ValueError :
+        pass
+        
+    
+    
 
-    diff_in_days = current_date - input_date
-    return diff_in_days
+print(get_days_from_today('2023-01-14'))
--- task_#2.py
+++ task_#2.py
@@ -4,11 +4,19 @@ import random
 def get_numbers_ticket(min, max, quantity):
 
     lottery_numbers = set()
+    condition = not( min < 1 or max > 1000 or quantity <= 0 or quantity > max - min or min > max) 
+    if condition :
+        try:
+            while len(lottery_numbers) != quantity:
+                lottery_numbers.add(random.randint(min, max))
+        except ValueError :
+            pass
+                
+    return lottery_numbers
 
-    if min < 1 or max > 1000 or quantity <= 0:
-        return lottery_numbers
+    
 
-    while len(lottery_numbers) != quantity:
-        lottery_numbers.add(random.randint(min, max))
+    
+    
 
-    return sorted(lottery_numbers)
+print(get_numbers_ticket(5,9,3))
